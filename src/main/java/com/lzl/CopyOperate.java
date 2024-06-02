a5d4a25735ead248958d12dd8b00f0ff7c120029
lizhengliang1226 <1731415266@qq.com>
2024-06-03 00:02:05 +0800

    使用内存映射进行复制

--- src/main/java/com/lzl/CopyOperate.java
+++ src/main/java/com/lzl/CopyOperate.java
@@ -53,45 +53,59 @@ public class CopyOperate implements FileOperate {
             LocalDateTime bgn = LocalDateTime.now();
             Log.get().info("文件{}开始复制，开始时间：{}", name, bgn.format(formatter));
             File tagFile = getTagFile(tagPath, name);
-            long length = srcFile.length();
-            // 计算块数
-            long blockCount = (length + BLOCK_SIZE - 1) / BLOCK_SIZE;
 //            ProgressBar progressBar = ProgressBar.builder().setTaskName(name).setInitialMax(blockCount).build();
             // 计算每块的开始地址和结束地址，存储在List<Pair>中
-            List<Pair<Long, Long>> blockRangePairs = IntStream.range(0, (int) blockCount).parallel().mapToObj(i -> {
-                long start = BLOCK_SIZE * i;
-                long end = Math.min(start + BLOCK_SIZE, length);
-                return Pair.of(start, end);
-            }).toList();
+            List<Pair<Long, Long>> blockRangePairs = getBlockRangePairs();
             // 并行遍历每块，并异步执行
-            List<CompletableFuture<String>> tasks = blockRangePairs.parallelStream().map(pair -> {
-                long start = pair.getKey();
-                long end = pair.getValue();
-                int blockNo = blockRangePairs.indexOf(pair);
-                CopyFileThread task = new CopyFileThread(srcFile, tagFile, start, end, blockNo);
-                CompletableFuture<String> taskFuture = CompletableFuture.supplyAsync(task, pool);
-                taskFuture.whenComplete((res, ex) -> {
-                    if (ex!= null) {
-                        ex.printStackTrace();
-                        Log.get().error("文件{}复制发生了异常，异常信息：{}", name, ex.getMessage());
-                    }
-//                    progressBar.step();
-//                    progressBar.refresh();
-                    Log.get().info(res);
-                });
-                return taskFuture;
-            }).toList();
+            List<CompletableFuture<String>> tasks = blockRangePairs.parallelStream()
+                                                                   .map(pair -> runTaskAsync(tagFile, blockRangePairs.indexOf(pair), pair))
+                                                                   .toList();
             CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0])).thenAccept((i) -> {
-                LocalDateTime end = LocalDateTime.now();
-                Log.get().info("文件{}结束复制，结束时间：{}", name, end.format(formatter));
-                Duration duration = Duration.between(bgn, end);
-                Log.get().info("总耗时：{}秒", duration.getSeconds());
-                pool.shutdown();
+                whenAllTaskComplate(name, bgn);
             });
         } catch (Exception e) {
             e.printStackTrace();
             Log.get().error("文件{}复制发生了异常，异常信息：{}", srcFile.getName(), e.getMessage());
         }
+    }
+
+    private void whenAllTaskComplate(String name, LocalDateTime bgn) {
+        LocalDateTime end = LocalDateTime.now();
+        Log.get().info("文件{}结束复制，结束时间：{}", name, end.format(formatter));
+        Duration duration = Duration.between(bgn, end);
+        Log.get().info("总耗时：{}秒", duration.getSeconds());
+        pool.shutdown();
+    }
+
+    private CompletableFuture<String> runTaskAsync(
+            File tagFile,
+            int blockNo,
+            Pair<Long, Long> pair) {
+        long start = pair.getKey();
+        long end = pair.getValue();
+//        int blockNo = blockRangePairs.indexOf(pair);
+        CopyFileThread task = new CopyFileThread(srcFile, tagFile, start, end, blockNo);
+        CompletableFuture<String> taskFuture = CompletableFuture.supplyAsync(task, pool);
+        taskFuture.whenComplete((res, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                Log.get().error("文件{}复制发生了异常，异常信息：{}", srcFile.getName(), ex.getMessage());
+            }
+//                    progressBar.step();
+//                    progressBar.refresh();
+            Log.get().info(res);
+        });
+        return taskFuture;
+    }
+
+    private List<Pair<Long, Long>> getBlockRangePairs() {
+        long length = srcFile.length();
+        long blockCount = (length + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        return IntStream.range(0, (int) blockCount).parallel().mapToObj(i -> {
+            long start = BLOCK_SIZE * i;
+            long end = Math.min(start + BLOCK_SIZE, length);
+            return Pair.of(start, end);
+        }).toList();
     }
 
     private static File getTagFile(String tagPath, String name) throws IOException {
