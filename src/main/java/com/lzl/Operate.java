6532235bc05a8da3bff3b8054a33a7be9fd470e0
lizhengliang <1731415266@qq.com>
2025-01-05 12:45:54 +0800

    refactor(创建NFO):将创建 NFO 文件的逻辑抽离到单独的类中
    - 新增 CreateNfoOperate 类，专门处理创建 NFO 文件的操作
    - 从 Operate 类中移除创建 NFO 文件的相关代码
    - 通过这种方式，提高了代码的可维护性和可重用性

--- src/main/java/com/lzl/Operate.java
+++ src/main/java/com/lzl/Operate.java
@@ -2,10 +2,6 @@ package com.lzl;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -55,37 +51,7 @@ public enum Operate {
     }, CREATE_NFO("5", "create nfo") {
         @Override
         FileOperate getOpt(File file, Object... args) throws IOException, InterruptedException {
-            return () -> {
-                String name = file.getName();
-                String id = name.substring(0, name.lastIndexOf("."));
-                System.out.print("【" + name + "】开始创建NFO信息文件！\t");
-                // 根据args[0]在目标目录下创建args[0]目录
-                String tagPath = (String) args[1];
-                // 在args[0]目录下，根据文件名创建一个文件同名目录
-                String actorPath = tagPath + File.separator + args[0] + File.separator + id;
-                Path createPath = Paths.get(actorPath);
-                if (!Files.exists(createPath)) {
-                    try {
-                        createPath = Files.createDirectories(createPath);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                // 在文件名目录下，创建nfo，把文件移动到此处
-                String infoContent = TemplateConstant.TEMPLATE
-                        .replace(TemplateConstant.DESC, name + "的描述信息")
-                        .replace(TemplateConstant.ID, id)
-                        .replace(TemplateConstant.ACTOR, (CharSequence) args[0])
-                        .replace(TemplateConstant.TITLE, name + "标题信息");
-                try {
-                    Files.writeString(createPath.resolve(id + ".nfo"), infoContent,
-                            StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-                    boolean b = file.renameTo(createPath.resolve(name).toFile());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("创建NFO成功！");
-            };
+            return new CreateNfoOperate(file, args);
         }
     };
     /**
